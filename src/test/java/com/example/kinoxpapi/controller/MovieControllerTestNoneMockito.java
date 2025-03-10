bfca1d1b9ea38050616d1740f54f75dc23f2fb7d
MadsLindersson <Mali0002@stud.kea.dk>
2025-03-10 10:58:43 +0100

    Fixed annoying shit.

--- src/test/java/com/example/kinoxpapi/controller/MovieControllerTestNoneMockito.java
+++ src/test/java/com/example/kinoxpapi/controller/MovieControllerTestNoneMockito.java
@@ -1,21 +1,12 @@
 package com.example.kinoxpapi.controller;
 
-import com.example.kinoxpapi.KinoXpApiApplication;
 import com.example.kinoxpapi.model.Movie;
 import com.example.kinoxpapi.repository.MovieRepository;
-import com.example.kinoxpapi.service.MovieService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
@@ -23,12 +14,11 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ActiveProfiles("test")
 @SpringBootTest
-public class MovieControllerTestNoneMickito {
+public class MovieControllerTestNoneMockito {
 
     @Autowired
     MovieController movieController;
